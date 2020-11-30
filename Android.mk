401a991fc8c96873377a5090b68e5f795df2261e
althafvly <althafvly@gmail.com>
2020-11-30 12:42:34 +0530

    Update to MGC 7.6.008 Parrot043 V8

--- Android.mk
+++ Android.mk
@@ -1,1 +1,13 @@
-include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCamera
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := GoogleCamera.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_PRODUCT_MODULE := true
+LOCAL_DEX_PREOPT := false
+LOCAL_OVERRIDES_PACKAGES := Camera2 Snap
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
